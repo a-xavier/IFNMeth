900ec7823eecb5cdc29eb429f2d02acdb15e5562
a-xavier <43228621+a-xavier@users.noreply.github.com>
2023-05-15 16:08:06 +1000

    Add files via upload

---
+++ R/Example_Data_Matrix.R
@@ -0,0 +1,14 @@
+#' Example Beta Matrix from the IFNMeth package
+#'
+#' @docType data
+#'
+#' @usage data(Example_Data_Matrix)
+#'
+#' @format An object of class RDa
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(Example_Data_Matrix)
+"Example_Data_Matrix"
---
+++ R/ifn_reference_cpgs.R
@@ -0,0 +1,14 @@
+#' Reference weights for CpGs used to create IFN methylation treatment score
+#'
+#' @docType data
+#'
+#' @usage data(ifn_reference_cpgs)
+#'
+#' @format An object of class RDa
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(ifn_reference_cpgs)
+"ifn_reference_cpgs"
