1fc3b8b7842a24a67767729b3af10c3bdca54116
a-xavier <43228621+a-xavier@users.noreply.github.com>
2023-05-24 11:04:27 +1000

    Add files via upload

---
+++ R/data_load.R
@@ -0,0 +1,11 @@
+#' @name Example_Data_Matrix
+#' @docType data
+#' @usage data(Example_Data_Matrix)
+#' @format A data frame
+NULL
+
+#' @name ifn_reference_cpgs
+#' @docType data
+#' @usage data(ifn_reference_cpgs)
+#' @format A data frame
+NULL
